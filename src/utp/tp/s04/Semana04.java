6d9c57a95c7452fb48dccd6bf83f64164b73aa64
UlisesChacon <uliseschacon96@gmail.com>
2024-04-22 20:29:50 -0500

    New Hola

--- src/utp/tp/s04/Semana04.java
+++ src/utp/tp/s04/Semana04.java
@@ -16,6 +16,7 @@ public class Semana04 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo 123");
+        System.out.println("Hola mundo x2");
     }
     
 }
