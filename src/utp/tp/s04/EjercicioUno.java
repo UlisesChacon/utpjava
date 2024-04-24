e1047772a77a1d5de54c7a45b071e2af77064299
UlisesChacon <uliseschacon96@gmail.com>
2024-04-24 11:48:22 -0500

    Ejercicios agregados S03 y S04 .

---
+++ src/utp/tp/s04/EjercicioUno.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utp.tp.s04;
+import java.util.Scanner;
+
+/**
+ *
+ * @author USULISES
+ * Ejercicio 1
+ * Escribir un programa que lea un número entero entre 1 y 50 y lo escriba en 
+ * numero romanos.
+ */
+public class EjercicioUno {
+    Scanner sc = new Scanner(System.in);
+    
+}
